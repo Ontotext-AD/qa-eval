b66762705ee45dfa074c2a8dd764fd76ae42900e
Philip Ganchev <phil.ganchev@graphwise.ai>
2025-07-17 15:54:24 +0300

    Minor

--- tests/steps/test_retrieval_evaluation.py
+++ tests/steps/test_retrieval_evaluation.py
@@ -6,17 +6,12 @@ from qa_eval.steps.retrieval import recall_at_k, average_precision
 def test_recall_at_k() -> None:
     relevant_items = {1, 3, 5, 7, 9}
     retrieved_items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-    k_value = 5
-    r_at_k = recall_at_k(relevant_items, retrieved_items, k_value)
-
-    assert math.isclose(r_at_k, 0.6)
+    r = recall_at_k(relevant_items, retrieved_items, 5)
+    assert math.isclose(r, 0.6)
 
 
 def test_average_precision() -> None:
-    relevant_q1 = {1, 2, 5, 8}
-    retrieved_q1 = [1, 3, 2, 4, 5, 6, 7, 8]
-
-    ap_score = average_precision(relevant_q1, retrieved_q1)
-
-    assert math.isclose(ap_score, 0.6916666666666667)
+    relevant_items = {1, 2, 5, 8}
+    retrieved_items = [1, 3, 2, 4, 5, 6, 7, 8]
+    p = average_precision(relevant_items, retrieved_items)
+    assert math.isclose(p, 0.6916666666666667)
