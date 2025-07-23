ead9f2fa9968c0a787c264b55ab7415c32455ef3
Philip Ganchev <phil.ganchev@graphwise.ai>
2025-07-24 00:07:07 +0300

    WIP More stable output format, but #tp > #p

--- prompts/template.md
+++ prompts/template.md
@@ -1,4 +1,7 @@
-Given a question, a reference answer and a candidate answer to it, extract all the claims from each answer, find corresponding claims between answers. Pay attention to details such as numbers, command names and paths. Output the values listed below, in JSON.
+Below is a question, a reference answer and a candidate answer to it.
+1. Extract all the claims from each answer
+2. Find matching claims between answers. Matching claims have the same meaning and details such as numbers, command names and paths.
+3. Output the values listed below.
 
 # Question
 {question}
@@ -10,15 +13,17 @@ Given a question, a reference answer and a candidate answer to it, extract all t
 {candidate_answer}
 
 # Output values
-v1: Numbered list of reference answer claims
-v2: Numbered list of candidate answer claims, each followed by its matching reference answer claim number (if any), in square brackets
-v3: Count of reference answer claims
-v4: Count of candidate answer claims
-v5: Count of matching claims
+* v1: List of reference answer claims
+* v2: List of candidate answer claims. Each claim string ends with the matching reference claim's 1-based index in square brackets, only if there is a such a match
+* v3: Count of reference answer claims
+* v4: Count of candidate answer claims
+* v5: Count of matching claims
 
-Output the values in JSON without markup.
-
+# Output format
+* JSON without markup
+  * v1, v2 values are lists of strings
 
 # Value checks
-1 <= v3, v4
-0 <= v5 <= v3, v4
+* v1, v2 values are valid JSON lists of strings
+* 1 <= v3, v4
+* 0 <= v5 <= v3, v4
--- qa_eval/answer_evaluation.py
+++ qa_eval/answer_evaluation.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import sys
+from io import StringIO
 from pathlib import Path
 
 from openai import OpenAI
@@ -27,6 +28,31 @@ def call_llm(client, prompt) -> str:
         return str(e).replace('\n', '    ')
 
 
+def _format_list(vals) -> str:
+    out_f = StringIO()
+    for i, v in enumerate(vals):
+        if i > 0:
+            out_f.write('\n')
+        cleaned = v.strip().replace('"', '\\"')
+        out_f.write(f'{1 + i}: {cleaned}')
+    return out_f.getvalue()
+
+
+def format_table_row(llm_output) -> tuple[str, str, str, str, str]:
+    try:
+        out_vals = json.loads(llm_output)
+    except json.JSONDecodeError as e:
+        print(llm_output)
+        raise e
+    return (
+        _format_list(out_vals['v1']),
+        _format_list(out_vals['v2']),
+        out_vals['v3'],
+        out_vals['v4'],
+        out_vals['v5']
+    )
+
+
 def evaluate_answers():
     client = OpenAI()
     with open(PROMPT_FILE_PATH, 'r', encoding='utf-8') as f:
@@ -36,7 +62,8 @@ def evaluate_answers():
         Path(OUT_FILE_PATH).parent.mkdir(parents=True, exist_ok=True)
         print(f'Writing results to {OUT_FILE_PATH}')
         with open(OUT_FILE_PATH, 'w', encoding='utf-8') as out_f:
-            out_f.write('\t'.join(OUT_FIELDS) + '\n')
+            writer = csv.writer(out_f, delimiter='\t')
+            writer.writerow(OUT_FIELDS)
             for row in reader:
                 prompt = prompt_template.format(
                     question=row['Question'],
@@ -44,20 +71,7 @@ def evaluate_answers():
                     candidate_answer=row['Actual answer'],
                 )
                 llm_output = call_llm(client, prompt)
-                # out_f.write(llm_output + '\n')
-                # for f in llm_output.split('\n'):
-                #     out_f.write(f'"{f}"\t')
-                # out_f.write('\n')
-                try:
-                    out_vals = json.loads(llm_output)
-                except json.JSONDecodeError as e:
-                    print(llm_output)
-                    raise e
-                out_f.write('"' + '\n'.join(str(v) for v in out_vals['v1']) + '"\t')
-                out_f.write('"' + '\n'.join(str(v) for v in out_vals['v2']) + '"\t')
-                out_f.write(f"{out_vals['v3']}\t")
-                out_f.write(f"{out_vals['v4']}\t")
-                out_f.write(f"{out_vals['v4']}\n")
+                writer.writerow(format_table_row(llm_output))
                 out_f.flush()
                 print('.', end='')
                 sys.stdout.flush()
