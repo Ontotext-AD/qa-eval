40d993e7f68a2de964f74e2b79bddcb049d320fd
Philip Ganchev <phil.ganchev@graphwise.ai>
2025-07-29 16:44:28 +0300

    Use csv instead of Pandas

--- qa_eval/answer_evaluation.py
+++ qa_eval/answer_evaluation.py
@@ -1,7 +1,7 @@
+import csv
 import sys
 from pathlib import Path
 
-import pandas
 from openai import OpenAI
 from tqdm import tqdm
 
@@ -30,22 +30,27 @@ def evaluate_answers():
     client = OpenAI()
     with open(PROMPT_FILE_PATH, 'r', encoding='utf-8') as f:
         prompt_template = f.read()
-    references = pandas.read_table(DATA_FILE_PATH)
     Path(OUT_FILE_PATH).parent.mkdir(parents=True, exist_ok=True)
     print(f'Writing results to {OUT_FILE_PATH}')
-    with open(OUT_FILE_PATH, 'w', encoding='utf-8') as out_f:
-        out_f.write('\t'.join(OUT_FIELDS) + '\n')
-        for i, row in tqdm(references.iterrows()):
-            prompt = prompt_template.format(
-                question=row['Question'],
-                reference_answer=row['Reference answer'],
-                candidate_answer=row['Actual answer'],
-            )
-            llm_output = call_llm(client, prompt)
-            out_f.write(llm_output + '\n')
-            out_f.flush()
-            print('.', end='')
-            sys.stdout.flush()
+    with open(DATA_FILE_PATH, encoding='utf-8') as in_f:
+        reader = csv.DictReader(in_f, delimiter='\t')
+        Path(OUT_FILE_PATH).parent.mkdir(parents=True, exist_ok=True)
+        print(f'Writing results to {OUT_FILE_PATH}')
+        with open(OUT_FILE_PATH, 'w', encoding='utf-8') as out_f:
+            out_f.write('\t'.join(OUT_FIELDS) + '\n')
+            rows = [row for row in reader]
+            for row in tqdm(rows):
+                prompt = prompt_template.format(
+                    question=row['Question'],
+                    reference_answer=row['Reference answer'],
+                    candidate_answer=row['Actual answer'],
+                )
+                llm_output = call_llm(client, prompt)
+                out_f.write(llm_output + '\n')
+                out_f.flush()
+                print('.', end='')
+                sys.stdout.flush()
+        print()
 
 
 if __name__ == '__main__':
